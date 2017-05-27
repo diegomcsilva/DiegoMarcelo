7f65a5abdb1302f74c38c8ba5236f7594f6040b9
Diego Marcelo <diegomcsilva191@gmail.com>
2017-05-27 00:52:47 -0300

    start

---
+++ index.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="public/css/style.min.css" rel="stylesheet">
+  </head>
+  <body>
+    <div class="header" id="header">
+      <header>
+      <div class="name">
+        <h1>Diego Marcelo</h1>
+        <div className="nav">
+          <ul>
+            <li><a href="#banner">Eu</a></li>
+            <li><a href="#about">Objetivo</a></li>
+            <li><a href="#competence">Competnencias</a></li>
+            <li><a href="#experiences">Experiências</a></li>
+            <li><a href="#"></a></li>
+          </ul>
+        </div>
+      </div>
+      </header>
+      <div className="banner" id="banner">
+        <div className="bannerLeft">
+        </div>
+        <div className="bannerRight">
+          <h1>Diego Marcelo Costa da Silva</h1>
+          <h2>Desenvolvedor Front End</h2>
+          <h3>Rio de Janeiro, Rio de Janeiro, Brasil</h3>
+        </div>
+      </div>
+    </div>
+    <div className="about" id="about">
+      <p>Meu objetivo é atuar na área de tecnologia da informação e desenvolver meus conhecimentos na área.
+        Minhas experiências me fizeram evoluir, mas sei que tenho muito para aprender.
+      </p>
+    </div>
+    <div className="competence" id="competence">
+      <div className="content">
+        <p>HTML5</p>
+        <div className="progress">
+          <div className="HTML5"></div>
+        </div>
+      </div>
+      <div className="content">
+        <p>CSS3</p>
+        <div className="progress">
+          <div className="CSS3"></div>
+        </div>
+      </div>
+      <div className="content">
+        <p>JS</p>
+        <div className="progress">
+          <div className="JS"></div>
+        </div>
+      </div>
+      <div className="content">
+        <p>React</p>
+        <div className="progress">
+          <div className="React"></div>
+        </div>
+      </div>
+      <div className="all">
+        Ionic – React – Metodologias Ágeis - Scrum – Kanban - TypesScript - SASS – Developer tools –
+        Gulp.js – AngularJS - HTML - HTML5 - CSS - Bootstrap - jQuery - Linux - Google Analytics -
+        Selenium Testing – Git - GitHub - CSS3 - Google Tag Manager - MySQL - Django - Google Doc -
+        Testes funcionais - Ms Office - Ms Excel - Ms PowerPoint – JavaScript - English - Inglês - Testes -
+        Liderança de equipe - Planejamento estratégico - Estratégia empresarial - Planejamento empresarial -
+        Relacionamento interpessoal
+      </div>
+    </div>
+    <div className="experiences" id="experiences">
+      <h2>Desenvolvedor Front End Junior</h2>
+      <h3>Empresa: <strong>Ventron</strong></h3>
+      <h3>Período: dez de 2016 – o momento  Duração do emprego6 meses</h3>
+      <h4>Localidade: Rio de Janeiro e Região, Brasil</h4>
+
+      <p>Atualmente estou atuando na empresa MEDGRUPO no desenvolvimento de apostilas para aplicativo mobile.</p>
+
+        <ul>
+          <li>- Metodologias Ágeis: Scrum, Kanban, DAKI etc.</li>
+          <li>- Angular</li>
+          <li>- Ionic</li>
+          <li>- Gulp</li>
+          <li>- Sass</li>
+          <li>- HTML5</li>
+          <li>- CSS3</li>
+          <li>- Javascript</li>
+          <li>- Git</li>
+          <li>- Metodologia Bem</li>
+        </ul>
+      </div>
+  </body>
+</html>
